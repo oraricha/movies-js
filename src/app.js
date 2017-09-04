74a0bfc2ef1ff2615906c7c7c737906451331842
oraricha <orarich@gmail.com>
2017-09-04 14:36:51 +0300

    added modal data & more css

--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
-// var app = (function() {
-//
-// })();
-
 var MovieService = (function() {
     var api =  'http://api.tvmaze.com/';
     var searchApi = 'search/shows?q=';
@@ -22,7 +18,7 @@ var MovieService = (function() {
     return {
         getMovieData: function(query) {
             if (typeof query === 'undefined' || !query) {
-                return false;
+                return Promise.reject();
             } else {
                 var encodedQuery = encodeURIComponent(query);
                 return handleResponse(fetch(api + searchApi + encodedQuery), 'getMovieData');
@@ -43,36 +39,36 @@ var MovieService = (function() {
 })();
 
 var DomService = (function() {
+    var api;
     var elements = {};
+    var NO_IMAGE_PLACEHOLDER = 'http://static.tvmaze.com/images/no-img/no-img-portrait-text.png';
 
     var attachListeners = function(){
-        // elements.searchBox.addEventListener("keyup", function(event) {
-        //     event.preventDefault();
-        //     if (event.keyCode === 13) {
-        //         console.log('enter');
-        //         searchCallback();
-        //     }
-        // });
-        //
-        // elements.searchBtn.addEventListener("click", function(event) {
-        //     searchCallback();
-        //     console.log('clicked');
-        // });
+        elements.modalCloseBtn.addEventListener('click', function() {
+            api.hideModal();
+        });
     };
 
     var init = function(){
-        elements.castList = document.querySelector('.boxes');
-        // elements.searchBtn = document.querySelector('.search-btn');
-        console.log(elements);
+        elements.boxList = document.querySelector('.boxes');
+        elements.modalWrapper = document.querySelector('.modal-wrapper');
+        elements.modalContent = document.querySelector('.modal-content');
+        elements.modalCloseBtn = document.querySelector('.modal-close-btn');
+        elements.castList = elements.modalContent.querySelector('.cast-list');
+        elements.movieScore = elements.modalContent.querySelector('.score');
+        elements.movieGeners = elements.modalContent.querySelector('.genres');
 
         attachListeners();
     };
 
     init();
 
-    return {
+    api = {
+        /**
+         * empties the results box and inserts new search results items
+         * @param data {Array} - the results data from server
+         */
         appendSearchResults: function(data) {
-            var NO_IMAGE_PLACEHOLDER = 'http://static.tvmaze.com/images/no-img/no-img-portrait-text.png';
             var movieItems = data.map(function(movie) {
                 var listItem = document.createElement('li');
                 listItem.className = 'box';
@@ -81,18 +77,62 @@ var DomService = (function() {
                 var movieName = document.createElement('div');
                 movieName.className = 'movie-name';
                 movieName.innerHTML = movie.show.name;
+
                 listItem.appendChild(img);
                 listItem.appendChild(movieName);
+
+                listItem.addEventListener('click', function() {
+                    api.showModal(movie);
+                });
 
                 return listItem;
             });
 
-            elements.castList.innerHTML = '';
+            elements.boxList.innerHTML = '';
             movieItems.forEach(function(listItem) {
-                elements.castList.appendChild(listItem);
+                elements.boxList.appendChild(listItem);
             });
+        },
+        showModal: function(movieData) {
+            MovieService.getCastData(movieData.show.id).then(function(castData) {
+                var animationTimeout;
+                console.log(castData);
+
+                var movieName = elements.modalContent.querySelector('h3');
+                movieName.innerHTML = movieData.show.name;
+
+                var castItems = castData.map(function(castPerson) {
+                    var listItem = document.createElement('li');
+                    listItem.innerHTML = castPerson.person.name;
+                    return listItem;
+                });
+
+                elements.castList.innerHTML = '';
+                castItems.forEach(function(castPerson) {
+                    elements.castList.appendChild(castPerson);
+                });
+
+                elements.movieScore.innerHTML = 'Score: ' + movieData.score;
+                elements.movieGeners.innerHTML = movieData.show.genres.join(', ');
+
+                elements.modalWrapper.classList.add('shown');
+                animationTimeout = setTimeout(function() {
+                    elements.modalWrapper.classList.add('visible');
+                    clearTimeout(animationTimeout);
+                }, .3);
+
+            });
+        },
+        hideModal: function() {
+            elements.modalWrapper.classList.remove('visible');
+            animationTimeout = setTimeout(function() {
+                elements.modalWrapper.classList.remove('shown');
+                clearTimeout(animationTimeout);
+            }, .3);
         }
     };
+
+    return api;
 
 })();
 
@@ -106,74 +146,31 @@ var app = (function() {
             MovieService.getMovieData(searchQuery)
                 .then(function(data) {
                     DomService.appendSearchResults(data);
-                });
+                })
+                .catch(function(error) { console.error('Error getting movies data.'); });
         }
     };
 
     var attachListeners = function(){
+        // enable search on enter
         elements.searchBox.addEventListener("keyup", function(event) {
             event.preventDefault();
             if (event.keyCode === 13) {
-                console.log('enter');
                 searchCallback();
             }
         });
 
         elements.searchBtn.addEventListener("click", function(event) {
             searchCallback();
-            console.log('clicked');
         });
     };
 
     var init = function(){
         elements.searchBox = document.querySelector('.search-input');
         elements.searchBtn = document.querySelector('.search-btn');
-        console.log(elements);
-
         elements.searchBox.focus();
-
         attachListeners();
     };
 
     init();
-
-
-
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
